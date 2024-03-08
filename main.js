dc9bf67c265065c3809059ead368c3cb8fc5503b
Natasa90 <natasa.golubovic90@gmail.com>
2024-03-08 17:38:25 +0100

    Added hover effect on button

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const button = document.getElementById("button-download")
 
 button.addEventListener("click", function onClick () {
-
 button.style.backgroundColor = "blue";
 
+});
+button.addEventListener("mouseover", () => {
+    button.style.backgroundColor = "#d93838";
+  });
+
+button.addEventListener("mouseout", () => {
+    button.style.backgroundColor = "#FF6464";
 });
